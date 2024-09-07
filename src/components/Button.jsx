99a03d1ce1feea3d69bab1dd8698620ebe4ff039
Karthikeyan <165583967+Karthikeyan8296@users.noreply.github.com>
2024-09-07 09:01:09 +0530

    Button Click Events

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 const Button = () => {
+  const handleClick1 = () => console.log("oops");
+  const handleClick2 = (name) => console.log(`${name} stop clicking me`);
+
+  //e == event parameter//
+  const handleClick = (e) => (e.target.textContent = "gomma");
+
   return (
     <div>
-      <button>vannko</button>
+      <button onClick={(e) => handleClick(e)}>Click Me </button>
     </div>
   );
 };
