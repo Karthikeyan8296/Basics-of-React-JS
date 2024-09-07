09c279465589c32f8f04f9f3e161877253505191
Karthikeyan <165583967+Karthikeyan8296@users.noreply.github.com>
2024-09-07 10:28:23 +0530

    UseState Basics

--- src/components/UseStateComponent.jsx
+++ src/components/UseStateComponent.jsx
@@ -1,16 +1,32 @@
 import { React, useState } from "react";
 
 const UseStateComponent = () => {
-  let [name, setName] = useState("Guest");
+  const [name, setName] = useState("Guest");
+  const [age, setAge] = useState(0);
+  const [isEmployed, setIsEmployed] = useState(false);
 
-  const updateName = () => {
+  const UpdateName = () => {
     setName("Karthik");
+  };
+
+  const IncrementAge = () => {
+    setAge(age + 1);
+  };
+
+  const toggeleEmployedstatus = () => {
+    setIsEmployed(!isEmployed);
   };
 
   return (
     <div>
       <p>Name: {name}</p>
-      <button onClick={updateName}>Set Name</button>
+      <button onClick={UpdateName}>Set Name</button>
+
+      <p>Age: {age}</p>
+      <button onClick={IncrementAge}>Increment Age</button>
+
+      <p>Is Employed: {isEmployed ? "Yes" : "No"}</p>
+      <button onClick={toggeleEmployedstatus}>Employed status</button>
     </div>
   );
 };
