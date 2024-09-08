4d8136807b5b716533018c0d490367855a1fad15
Karthikeyan <165583967+Karthikeyan8296@users.noreply.github.com>
2024-09-08 18:14:05 +0530

    Digital Clock

---
+++ src/AppClock.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DigitalClock from "./components/DigitalClock";
+
+const AppClock = () => {
+  return (
+    <>
+      <DigitalClock />
+    </>
+  );
+};
+
+export default AppClock;
---
+++ src/components/DigitalClock.jsx
@@ -0,0 +1,42 @@
+import { React, useEffect, useState } from "react";
+import "./DigitalClock.css";
+
+const DigitalClock = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, []);
+
+  function formatTime() {
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+    const meridiem = hours >= 12 ? "PM" : "AM";
+
+    hours = hours % 12 || 12;
+
+    return `${padZero(hours)} : ${padZero(minutes)} : ${padZero(seconds)} ${meridiem}`;
+  }
+
+  //adding zero in front//
+  function padZero(number) {
+    return (number < 10 ? "0" : "") + number;
+  }
+
+  return (
+    <div className="clock-container">
+      <div className="clock">
+        <span>{formatTime()}</span>
+      </div>
+    </div>
+  );
+};
+
+export default DigitalClock;
